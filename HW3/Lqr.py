fa918e0cba16929d6b661a6b492ddc5a8ec87259
Alokaado <temesgen.ks.mehari@gmail.com>
2017-01-26 14:59:27 +0100

    Lqr.py modified

--- HW3/Lqr.py
+++ HW3/Lqr.py
@@ -1,13 +1,35 @@
 import numpy as np
-import matplotlib.pyplot as plt
-def  lqr():
-    A = np.array([[1, 0.1][0, 1]])
-    B = np.array([[0][0.1]])
-    b = np.array([[5][0]])
+import matplotlib.pyplot as plta
+
+def  lqr(startstate):
+    #LQR parameter
+    A_t = np.array([[1, 0.1],[0, 1]])
+    B_t = np.array([[0],[0.1]])
+    b_t = np.array([[5],[0]])
     Sig_t = 0.01
     K_t = np.array([5, 0.3])
     k_t  =0.3
     H_t = 1
+    T = 50
+    
+    states = np.zeros((2,T))
+    states[:,0] = startstate
+    actions = np.zeros(T)
+    for i in range(1,T): 
+        w_t = np.random.normal(b_t,Sig_t)
+        actions[i-1] = -1.0 * np.dot(K_t,states[:,i-1]) + k_t
+        states[:,i] = np.dot(A_t,states[:,i-1]) + B_t * actions[i-1] + w_t
+
+    return actions, states
+def compute_rt(s_t,a_t,H_t,t,T): 
+    r_t = getr_t(t)
+    R_t = getR_t(t)
+    diff = s_t - r_t
+    rslt = -1.0 *np.dot(np.dot(np.transpose(diff),R_t),diff)    
+    if (t == T): 
+        return rslt
+    else: 
+        return rslt - np.dot(np.dot(np.transpose(a_t),H_t),a_t)
 
 
 def getR_t(t):
