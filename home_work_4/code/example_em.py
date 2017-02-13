2f357e23ea17475a5a046a17f763ed3555ae1af0
Akrenion <Akrenion@users.noreply.github.com>
2017-02-13 16:03:34 +0100

    Add files via upload

--- home_work_4/code/example_em.py
+++ home_work_4/code/example_em.py
@@ -5,74 +5,66 @@ from Pend2dBallThrowDMP import *
 
 
 numDim = 10
-numSamples = 25
-maxIter = 100
+numSamples = 30
+maxIter = 300
 numTrials = 10
 lambda_var = 7
-i = 0
-Mu_w = np.zeros(numDim)
-Sigma_w = np.eye(numDim) * 1e6
 
 env = Pend2dBallThrowDMP()
 
 def main():
 	# Do your learning
-	(q, u, b, bd) = env.simulateSystem(np.array([[2], [1]]))
-
 	# For example, let initialize the distribution...
 	Mu_w = np.zeros(numDim)
-	Sigma_w = np.eye(numDim) * 1e6
-
+	Sigma_w = np.eye(numDim) * 1e5
 	# ... then draw a sample and simulate an episode
-	sample = mkSample()
+	sample = mkSample(Mu_w, Sigma_w)
 	reward = rewardSample(sample)
 	beta = mkBeta(reward)
 	weights = mkWeight(reward, beta)
 	Mu_w = mkWeightedMean(sample, weights, reward)
 	Sigma_w = mkWeightedCovariance(sample, weights ,Mu_w)
-	
+	mu = np.copy(Mu_w)
+	sig = np.copy(Sigma_w)
 	i = 1
 	convergence = False
-	while(i < 100 and not convergence):
+	while(i < maxIter and not convergence):
 		i += 1
-		if i is 100: print "convergence not reached"
+		if i is maxIter: print "convergence not reached"
 		old_reward = reward
-
-		sample = mkSample()
+		sample = mkSample(mu, sig)
 		reward = rewardSample(sample)
 		beta = mkBeta(reward)
 		weights = mkWeight(reward, beta)
-		Mu_w = mkWeightedMean(sample, weights, reward)
-		Sigma_w = mkWeightedCovariance(sample, weights ,Mu_w)
-		
-		convergence = True if meanRewardDiff(reward, old_reward) < 1e3 else False
-
+		mu = mkWeightedMean(sample, weights, reward)
+		sig = mkWeightedCovariance(sample, weights ,mu) + np.eye(numDim)
+		convergence = True if meanRewardDiff(reward, old_reward) < 1e-3 else False
+      
 	# Save animation
-	print Mu_w
-	env.animate_fig(np.random.multivariate_normal(Mu_w, Sigma_w))
+	env.animate_fig(np.random.multivariate_normal(mu, sig))
 	plt.show()
 	# plt.savefig('EM-Ex2.pdf')
 
 
 def mkBeta(reward):
-	return lambda_var / (np.max(reward) + 1 - np.min(reward))
+	return lambda_var / (np.amax(reward) - np.amin(reward))
 
 def mkWeight(reward, beta):
 	weight = np.zeros(numSamples)
-	for current in weight:	
+	for current in range(numSamples):	
 		weight[current] = np.exp((reward[current] - np.max(reward)) * beta)
 	return weight
 
-def mkSample():
-    sample = np.zeros(numSamples)
+def mkSample(mu, sig):
+    sample = np.zeros((numDim, numSamples))
     for i in range(numSamples):
-        sample = np.random.multivariate_normal(Mu_w, Sigma_w)
+        sample[:,i] = np.random.multivariate_normal(mu, sig)
     return sample
 
 def rewardSample(sample):
     reward = np.zeros(numSamples)
     for i in range(numSamples):
-        reward[i] = env.getReward(sample)
+        reward[i] = env.getReward(sample[:,i])
     return reward
 
 def meanRewardDiff(rew1, rew2):
@@ -80,19 +72,19 @@ def meanRewardDiff(rew1, rew2):
 
 def mkWeightedMean(sample, weight,reward):
     sumOfWeights = 0
-    sumOfWeightedSamples = 0
-    for i in range(sample.shape[0]):
+    sumOfWeightedSamples = 0    
+    for i in range(numSamples):
      	sumOfWeights += weight[i]
-        sumOfWeightedSamples += weight[i] * sample[i]
-    return sumOfWeights / sumOfWeightedSamples
-
+        sumOfWeightedSamples += weight[i] * sample[:,i]
+    return sumOfWeightedSamples / sumOfWeights
+    
 def mkWeightedCovariance(sample, weights,mean):
     sumOfWeights = 0
     sumOfWeightedSample = 0
     for i in range(sample.shape[0]):
         sumOfWeights += weights[i]
-        sumOfWeightedSample += weights[i] * (sample[i] - mean) * np.transpose(sample[i] - mean)
-    return sumOfWeightedSample / sumOfWeights
+        sumOfWeightedSample += np.dot(weights[i], np.dot((sample[:,i] - mean), np.transpose(sample[:,i] - mean)))
+    return np.dot((sumOfWeightedSample / sumOfWeights), np.eye(numDim))
 
 if __name__ == '__main__':
     main()
